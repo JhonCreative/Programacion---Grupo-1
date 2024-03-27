842fe71265986c4b2cbcc9a4679f2498ad980811
Prueba 1 <calsinaparaya@email.com>
2024-03-26 22:55:51 -0400

    add group list

--- Java/Lab 1 - Clase instrumentos/src/App.java
+++ Java/Lab 1 - Clase instrumentos/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Grupo 1");
+        System.out.println("Alex");
+        System.out.println("Jhoselyn");
+        System.out.println("Benjamin");
+        System.out.println("Jhon");
+        System.out.println("Juanjo");
+        System.out.println("Duglas");
     }
 }
